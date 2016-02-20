1fd23e59aff9a36fe73ea1f5991e48d72a8cc2e1
akojimaoz <akira.ikmnhv6@gmail.com>
2016-02-20 16:11:45 +0900

    PlayerManager 変更

--- Assets/Scripts/Game/PlayerController.cs
+++ Assets/Scripts/Game/PlayerController.cs
@@ -3,13 +3,18 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 	public float speed = 1.0f;
+	public float jumpSpeed = 10f;
+	Rigidbody2D rb;
+
 	// Use this for initialization
 	void Start () {
-	
+		rb = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetButtonDown ("fire1")) {
+			rb.velocity = new Vector2 (rb.velocity.x, jumpSpeed); 
+		}
 	}
 }
