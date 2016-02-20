676ceca573893c2f10c1e3bd4235296aa60aa0a0
hiroyasu yamazaki <mmk.hrys0403@gmail.com>
2016-02-20 21:47:01 +0900

    TitleとResultのManager追加

---
+++ Assets/Scripts/Result/ResultManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResultManager : MonoBehaviour {
+
+	void Start () {
+	
+	}
+
+	void Update () {
+        if (Input.GetButtonDown("Fire1")) {
+            Application.LoadLevel("Title");
+        }
+	}
+}
