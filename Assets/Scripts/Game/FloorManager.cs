f4538ccbe48775a94e7666350973991961e625af
akojimaoz <akira.ikmnhv6@gmail.com>
2016-02-20 15:48:05 +0900

    make PlayerController FloorManager GameManager

---
+++ Assets/Scripts/Game/FloorManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorManager : MonoBehaviour {
+	public float speed = 1.0f;
+	public float startPosition;
+	public float endPosition;
+	
+	// Use this for initialization
+	void Update() {
+		//transform floor to left
+		transform.Translate(-1 * speed * Time.deltaTime, 0, 0);
+
+		if(transform.position.x <= endPosition) ScrollEnd();
+	}
+
+	void ScrollEnd(){
+		transform.Translate (-1 * (endPosition - startPosition), 0, 0);
+	}
+}
---
+++ Assets/Scripts/Game/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed = 1.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
