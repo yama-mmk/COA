1b57ce9967fd9e32d37c5443919bd3307224a300
hiroyasu yamazaki <mmk.hrys0403@gmail.com>
2016-02-20 16:52:27 +0900

    VegetablrGenerator変更

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -1,32 +1,77 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour {
 
     // ゲームの進行管理
     public enum State {
         NONE = -1,     // 初期状態
-        READY = 0,     // 
-        PLAYING,
-        FINISH,
+        READY = 0,     // 開始状態
+        PLAYING,       // プレイ中
+        FINISH,        // 終了状態
     };
+
+    public State state = State.NONE;
 
     // ゲームの難しさ
     public enum LEVEL {
-        NONE = -1,
-        EASY = 0,
-        NORMAL,
-        HARD,
-        VERYHARD,
+        NONE = -1,     // 初期状態
+        EASY = 0,      // 簡単
+        NORMAL,        // 普通
+        HARD,          // 難しい
+        VERYHARD,      // 超難しい
     };
 
     public LEVEL level = LEVEL.NONE;
 
-	void Start () {
+    // 時間
+    private float time = 0.0f;
 
+    private VegetableGenerator generator;
+
+	void Start () {
+        // 初期化
+        state = State.READY;
+        level = LEVEL.EASY;
+        
+        time = 0.0f;
+
+        generator = GameObject.Find("VegetableGenerator").GetComponent<VegetableGenerator>();
 	}
 
 	void Update () {
-	
+        time += Time.deltaTime;
+
+        switch(state) {
+            case State.READY:
+
+                if (time >= 5.0f) {
+                    time = 0.0f;
+                    state = State.PLAYING;
+                }
+                break;
+            case State.PLAYING:
+
+                if (time >= 15.0f) {
+                    time = 0.0f;
+                    switch(level) {
+                        case LEVEL.EASY:
+                            generator.UpdateLevel(LEVEL.NORMAL);
+                            break;
+                        case LEVEL.NORMAL:
+                            generator.UpdateLevel(LEVEL.HARD);
+                            break;
+                        case LEVEL.HARD:
+                            generator.UpdateLevel(LEVEL.VERYHARD);
+                            break;
+                    }
+                }
+                break;
+            case State.FINISH:
+                break;
+            default:
+                break;
+        }
 	}
 }
--- Assets/Scripts/Game/VegetableGenerator.cs
+++ Assets/Scripts/Game/VegetableGenerator.cs
@@ -1,15 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class VegetableGenerator : MonoBehaviour {
 
-    public GameObject[] vegetables;
+    public List<GameObject> vegetables;
+
+    private GameManager manager;
+    private GameManager.LEVEL level;
 
 	void Start () {
-
+        // Level初期化
+        manager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        level = manager.level;
 	}
 
 	void Update () {
-
+        switch(level) {
+            case GameManager.LEVEL.EASY:
+                GenerateVegetable(7.0f);
+                break;
+            case GameManager.LEVEL.NORMAL:
+                GenerateVegetable(5.0f);
+                break;
+            case GameManager.LEVEL.HARD:
+                GenerateVegetable(4.0f);
+                break;
+            case GameManager.LEVEL.VERYHARD:
+                GenerateVegetable(3.0f);
+                break;
+        }
 	}
+
+    private IEnumerator GenerateVegetable(float time) {
+        yield return new WaitForSeconds(time);
+
+        int num = (int)Random.Range(0, vegetables.Count);
+        Instantiate(vegetables[num], new Vector2(12.0f, -2.0f), Quaternion.identity);
+    }
+
+    // レベル更新
+    public void UpdateLevel(GameManager.LEVEL l) {
+        this.level = l;
+    }
 }
