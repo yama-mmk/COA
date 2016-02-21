01f8cb69ea52dbb4233f316cb7eeb5fa72069e50
akojimaoz <akira.ikmnhv6@gmail.com>
2016-02-21 13:24:36 +0900

    ready time

--- Assets/Scripts/Game/BgFader.cs
+++ Assets/Scripts/Game/BgFader.cs
@@ -2,22 +2,22 @@
 using System.Collections;
 
 public class BgFader : MonoBehaviour {
-	private float fadeTime = 5f;
-	private VegetableGenerator generator;
-	private float level;
+	private float fadeTime = 20f;
+	private GameManager manager;
+	private float readyTime;
 	private float currentRemainTime;
 	private float time = 0.0f;
 	// Use this for initialization
 	void Start () {
-		generator = GameObject.Find("VegetableGenerator").GetComponent<VegetableGenerator>();
-		level = (int)(generator.level);
+		manager = GameObject.Find("GameManager").GetComponent<GameManager>();
+		readyTime = manager.READY_TIME;
 		currentRemainTime = fadeTime;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		time += Time.deltaTime;
-		if (time > 3.0f) {
+		if (time > readyTime) {
 			currentRemainTime -= Time.deltaTime;
 		
 			if (currentRemainTime <= 0f) {
